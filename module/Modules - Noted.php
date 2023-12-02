b2e0b29d3c3e4014b9e0e3d1c46ab9b5471203fa
ogisetiawan <ogisetiawan21@gmail.com>
2023-12-02 16:49:38 +0700

    ✅  add Security notes

--- module/Modules - Noted.php
+++ module/Modules - Noted.php
@@ -52,3 +52,46 @@
 //? CI (Continuous Integration); proses terintegrasinya sebuah aksi ketika developer mengirim (push) kode barunya ataupun melakukan pull request (PR) e branch utama pada repository 
 //? CD (Continuous Deployment). proses deployment aplikasi secara otomatis yang hanya akan dijalankan bila pengujian aplikasi berstatus passed
 //? Tools; travis CI, Jenkins, GitHub Action
+
+//@ Security
+//~ SQL Injection
+//? SQL Injection; merupakan serangan yang dilakukan melalui inputan atau payload dalam memanipulasi SQL
+/// SQL injection dalam bypass otentikasi
+//? menambahkan " or " pada user dan password
+//? SELECT * FROM users WHERE username = "" or ""="" AND password = "" or ""="" LIMIT 1;
+/// SQL injection Pencurian dan manipulasi
+//? menambahkan perintah query lainya dengan ditutup ;
+//? SELECT * FROM product WHERE category = "food; SELECT username, password FROM users; --
+/// Mencegah SQL Injection
+//? validasi input atau payload yang dikirimkan user sebelum diproses ke database
+//? Parameterized Statements dan Object Relational Mapping (ORM) 
+/// Parameterized Query
+//? merupakan teknik yang dapat mencegah terjadinya SQL injections. Karena melalui teknik tersebut, parameter atau variabel yang dilibatkan dalam membangun SQL akan diperlakukan secara aman
+//#   const sql = {
+//#     text: 'SELECT * from users WHERE username = $1 AND password = $2',
+//#     values: [username, password],
+//#   };
+/// Object Relational Mapping (ORM)
+//? menerjemahkan SQL dalam bentuk object dan cepat
+//~ Browser Security (CORS)
+//? kebijakan suatu website yang hanya dapat diakses dengan origin yang sama (protocol,host dan port)
+//? agar website tidak berinteraksi secara ilegal terhadap resource yang bukan haknya.
+//? contoh untuk web transaksi agar tdk di post ke origin lain (cross-origin)
+/// CORS dari Sisi Client
+//? response yang mengandung header Access-Control-Allow-Origin dengan nilai origin yang sama
+/// CORS dari Sisi Server
+//? tambahkan properti Access-Control-Allow-Origin dengan nilai origin yang diijinkan pada response header.
+//? jadi jika memang web membutuhkan origin yg sama, kita dapat set beberapa origin secara default.
+//~ Serangan Denial-of-Service (DoS)
+//? penyerang membanjiri aplikasi Anda dengan traffic jaringan yang masif sehingga membuatnya kewalahan dan tak lagi dapat merespons permintaan pengguna
+/// Distributed Denial-of-Service (DDoS)
+//? serangan DDoS menggunakan beberapa sumber untuk melakukan serangan, jika kita memblock ip pada DOS
+/// Langkah Preventif terhadap Serangan DDoS
+//? melimitasi request yang masuk atau request rate limit. Masing-masing client hanya dapat melakukan sejumlah request saja selama kurun waktu tertentu.
+/// Reverse proxy server
+//? sebagai perantara terhadap web server atau HTTP server lain.
+//? Tak hanya security, ia juga bisa meningkatkan kinerja, memudahkan scaling dan mengatur traffic masuk pada aplika
+//~  Man In The Middle
+//? serangan menyadap, tak hanya memantau, serangan ini dapat mencegat dan mengubah pesan yang dikirim atau pesan yang diterima
+/// IP spoofing atau ARP spoofing
+//?  karena korban dan penyerang berada di dalam jaringan yang sama. Biasanya disebabkan oleh korban yang tidak hati-hati ketika menggunakan WiFi hotspot publik
